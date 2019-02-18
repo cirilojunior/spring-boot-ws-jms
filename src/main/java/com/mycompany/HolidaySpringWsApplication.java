9a9146c4d8aa18e6c7e60619aeeff9e196518871
Cirilo L X Junior <cirilolxjunior@gmail.com>
2019-02-18 00:14:42 -0300

    documentação...

--- src/main/java/com/mycompany/HolidaySpringWsApplication.java
+++ src/main/java/com/mycompany/HolidaySpringWsApplication.java
@@ -2,11 +2,30 @@ package com.mycompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class HolidaySpringWsApplication extends SpringBootServletInitializer {
 
+    /**
+     * Para executar em containers da forma tradicional, ou seja,
+     * como WAR. Sobrescrevendo método de @see {@link SpringBootServletInitializer}.
+     *
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(HolidaySpringWsApplication.class);
+    }
+
+    /**
+     * Para rodar a aplicação via SpringBoot.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(HolidaySpringWsApplication.class, args);
     }
