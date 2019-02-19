9425398c56c2d1c2440bff679abb81786aefd2cc
cirilo.xavier <cirilo.xavier@mprj.mp.br>
2019-02-19 19:49:48 -0300

    ajustes nas configs JMS

--- src/main/java/br/com/minhaempresa/MessageriaConfig.java
+++ src/main/java/br/com/minhaempresa/MessageriaConfig.java
@@ -5,13 +5,10 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.support.converter.MessageConverter;
 
 import javax.jms.ConnectionFactory;
@@ -26,34 +23,17 @@ public class MessageriaConfig {
     @Value("${spring.activemq.broker-url}")
     private String brokerUrl;
 
-
     @Bean
-    public ActiveMQConnectionFactory processoEletronicoMessageFactory() {
-        ActiveMQConnectionFactory activeMQConnectionFactory =
-                new ActiveMQConnectionFactory();
-        activeMQConnectionFactory.setBrokerURL(brokerUrl);
-
-        return activeMQConnectionFactory;
+    public ConnectionFactory connectionFactory() {
+        return new ActiveMQConnectionFactory(brokerUrl);
     }
 
     @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
-        DefaultJmsListenerContainerFactory factory =
-                new DefaultJmsListenerContainerFactory();
-        factory
-                .setConnectionFactory(processoEletronicoMessageFactory());
-
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
         return factory;
     }
-
-//    @Bean
-//    public JmsListenerContainerFactory<?> processoEletronicoMessageFactory(ConnectionFactory connectionFactory,
-//                                                                           DefaultJmsListenerContainerFactoryConfigurer configurer) {
-//        Logger.info("Criando JMS Message Factory...");
-//        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-//        configurer.configure(factory, connectionFactory);
-//        return factory;
-//    }
 
     @Bean
     public MessageConverter documentoMessageConverter() {
